a152bfdda50a7fc267de4ac33edb3d2045e6c942
Praveen Premaratne <praveen.mayantha@gmail.com>
2025-01-02 19:39:00 +0000

    Script logging to console and log file

--- resources/consul/scripts/init.sh
+++ resources/consul/scripts/init.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-echo "Updating and patching system"
+set -e
+exec >> /var/log/consul_setup.log 2>&1
+
+logger() {
+  echo "$(date '+%Y/%m/%d %H:%M:%S') init.sh: $1"
+}
+
+logger "Updating and patching system"
 apt-get update
 apt-get upgrade -y
 
-echo "Installing basic tools"
+logger "Installing basic tools"
 apt-get install -y vim curl gpg
